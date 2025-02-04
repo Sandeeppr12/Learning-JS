1aea8631d76f690618070bc8e252f8af334eba37
Sandeeppr12 <161575610+Sandeeppr12@users.noreply.github.com>
2025-02-04 05:10:29 +0000

    add

---
+++ 01_basics/03_basics.js
@@ -0,0 +1,21 @@
+console.log("Sandeep");
+//Operations in js
+let value=3
+let negValue=-value
+console.log(negValue);
+console.log(2+2);
+console.log(2*2);
+console.log(5-2);
+console.log(8/2);
+
+let str1='rg'
+let str2='eq'
+let str3=str1+str2
+console.log(str3);
+console.log('1'+2);
+console.log(2+"1")
+console.log("1"+2+2);
+
+
+
+
