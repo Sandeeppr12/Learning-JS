f457c7a88dba8fea72456d585b87a49871781c41
Sandeeppr12 <161575610+Sandeeppr12@users.noreply.github.com>
2025-02-04 16:54:38 +0000

    new

--- 01_basics/03_basics.js
+++ 01_basics/03_basics.js
@@ -1,20 +1,54 @@
-console.log("Sandeep");
-//Operations in js
-let value=3
-let negValue=-value
-console.log(negValue);
-console.log(2+2);
-console.log(2*2);
-console.log(5-2);
-console.log(8/2);
+// console.log("Sandeep");
+// //Operations in js
+// let value=3
+// let negValue=-value
+// console.log(negValue);
+// console.log(2+2);
+// console.log(2*2);
+// console.log(5-2);
+// console.log(8/2);
 
-let str1='rg'
-let str2='eq'
-let str3=str1+str2
-console.log(str3);
-console.log('1'+2);
-console.log(2+"1")
-console.log("1"+2+2);
+// let str1='rg'
+// let str2='eq'
+// let str3=str1+str2
+// console.log(str3);
+// console.log('1'+2);
+// console.log(2+"1")
+// console.log("1"+2+2);
+// console.log(1+2+'3');
+// console.log("Sandeep");
+// console.log(true);
+// console.log(+true);
+// console.log(+'');
+// let name = 2;
+// name++;
+// console.log(name);
+// ++name;
+// console.log(name);
+console.log('2'>1);
+console.log('02'>1);
+
+console.log(null>0);
+console.log(null==0);
+console.log(null>=0);
+
+console.log(undefined==0);
+console.log(undefined>=0);
+console.log(undefined<=0);
+
+
+//===
+console.log('2'===2);
+
+
+
+
+
+
+
+
+
+
 
 
 
